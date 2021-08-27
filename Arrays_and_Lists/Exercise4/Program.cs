2e4845eb98015d06eb7eaf2c0ef0463e37f45243
lostinthestack <lostinthestack@protonmail.com>
2021-08-27 12:17:05 -0500

    Update Exercise4

--- Arrays_and_Lists/Exercise4/Program.cs
+++ Arrays_and_Lists/Exercise4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Exercise4
 {
@@ -6,7 +7,26 @@ namespace Exercise4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = new List<int>();
+            while (true)
+            {
+                System.Console.WriteLine("Enter a number or type 'QUIT' to exit: ");
+                var input = Console.ReadLine();
+                if (input == "QUIT".ToLower())
+                    break;
+                else
+                    numbers.Add(Convert.ToInt32(input));
+            }
+            numbers.Sort();
+            var uniques = new List<int>();
+            foreach (var number in numbers)
+            {
+                if (!uniques.Contains(number))
+                {
+                    uniques.Add(number);
+                    System.Console.WriteLine(number);
+                }
+            }
         }
     }
 }
