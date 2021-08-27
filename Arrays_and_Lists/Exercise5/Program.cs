c8c747c6e854904f13d573de4c8317156803f4cf
lostinthestack <lostinthestack@protonmail.com>
2021-08-27 13:05:22 -0500

    Comment cleanup

--- Arrays_and_Lists/Exercise5/Program.cs
+++ Arrays_and_Lists/Exercise5/Program.cs
@@ -8,13 +8,6 @@ namespace testcs
     {
         static void Main(string[] args)
         {
-            //
-            // ORIGINAL EXERCISE : 
-            //
-            // Write a program and ask the user to supply a list of comma separated numbers (e.g 5, 1, 9, 2, 10).
-            // If the list is empty or includes less than 5 numbers, display "Invalid List" 
-            // and ask the user to re-try; otherwise, display the 3 smallest numbers in the list. 
-            //
             var numList = new List<int>();
             while (true)
             {
