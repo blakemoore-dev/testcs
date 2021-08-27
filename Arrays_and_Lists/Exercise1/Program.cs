2f28e3ec8a4f39f488325e47782fc70e8f79fe5b
lostinthestack <lostinthestack@protonmail.com>
2021-08-27 12:36:19 -0500

    Update Exercise1

--- Arrays_and_Lists/Exercise1/Program.cs
+++ Arrays_and_Lists/Exercise1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Exercise1
 {
@@ -6,7 +7,24 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var likes = new List<string>();
+            while (true)
+            {
+                System.Console.WriteLine("Who likes your Facebook post?");
+                var input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                    break;
+                
+                likes.Add(input);
+            }
+            if (likes.Count == 0)
+                System.Console.WriteLine("Nobody likes you.");
+            else if (likes.Count == 1)
+                System.Console.WriteLine("{0} likes your post.", likes[0]);
+            else if (likes.Count == 2)
+                System.Console.WriteLine("{0} and {1} like your post.", likes[0], likes[1]);
+            else if (likes.Count > 2)
+                System.Console.WriteLine("{0], {1}, and {2} others like your post.", likes[0], likes[1], likes.Count - 2);
         }
     }
 }
