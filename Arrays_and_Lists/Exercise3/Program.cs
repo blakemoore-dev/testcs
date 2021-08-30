4949dba15ae8044323b3e1203f486a6f99bcd222
lostinthestack <lostinthestack@protonmail.com>
2021-08-30 11:07:24 -0500

    purge gitignore

---
+++ Arrays_and_Lists/Exercise3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+            while (numbers.Count < 5)
+            {
+                System.Console.WriteLine("Enter a number: ");
+                var input = Convert.ToInt32(Console.ReadLine());
+                if (!numbers.Contains(input))
+                    numbers.Add(input);
+                else
+                    System.Console.WriteLine("Number already exists. Try again...");
+            }
+            numbers.Sort();
+            foreach (var number in numbers)
+                System.Console.WriteLine(number);
+        }
+    }
+}
---
+++ Arrays_and_Lists/Exercise4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+            while (true)
+            {
+                System.Console.WriteLine("Enter a number or type 'QUIT' to exit: ");
+                var input = Console.ReadLine();
+                if (input == "QUIT".ToLower())
+                    break;
+                else
+                    numbers.Add(Convert.ToInt32(input));
+            }
+            numbers.Sort();
+            var uniques = new List<int>();
+            foreach (var number in numbers)
+            {
+                if (!uniques.Contains(number))
+                {
+                    uniques.Add(number);
+                    System.Console.WriteLine(number);
+                }
+            }
+        }
+    }
+}
