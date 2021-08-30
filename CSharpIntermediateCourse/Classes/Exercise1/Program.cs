a94ec95bb758c929cfdfaa5cdbfddd366ba7d189
lostinthestack <lostinthestack@protonmail.com>
2021-08-30 13:18:08 -0500

    Intro to Classes and Constructors

--- CSharpIntermediateCourse/Classes/Exercise1/Program.cs
+++ CSharpIntermediateCourse/Classes/Exercise1/Program.cs
@@ -6,7 +6,15 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "John";
+
+            var order = new Order();
+            customer.Orders.Add(order);
+
+            System.Console.WriteLine(customer.Name);
+            System.Console.WriteLine(customer.Id);
         }
     }
 }
