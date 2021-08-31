8605c74a8d3428b77ee7493a17dc9194862555f2
lostinthestack <lostinthestack@protonmail.com>
2021-08-30 19:43:16 -0500

    Add README for exercise1

---
+++ CSharpIntermediateCourse/Section2/Exercise1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise1
+{
+    public class StopWatch
+    {
+        public DateTime StartTime { get; private set; }
+
+        public void Start(DateTime startTime)
+        {
+            StartTime = startTime;
+        }
+
+        public void Stop()
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
