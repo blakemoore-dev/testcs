dd188c10e91c4bedd0dc5ba34142f48108c640f2
lostinthestack <lostinthestack@protonmail.com>
2021-09-01 14:25:20 -0500

    update

---
+++ Arrays_and_Lists/Exercise1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var likes = new List<string>();
+            while (true)
+            {
+                System.Console.WriteLine("Who likes your Facebook post?");
+                var input = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(input))
+                    break;
+                
+                likes.Add(input);
+            }
+
+            if (likes.Count > 2)
+                System.Console.WriteLine("{0}, {1} and {2} others like your post.", likes[0], likes[1], likes.Count - 2);
+            else if (likes.Count == 2)
+                System.Console.WriteLine("{0} and {1} like your post.", likes[0], likes[1]);
+            else if (likes.Count == 1)
+                System.Console.WriteLine("{0} likes your post.", likes[0]);
+            else
+                System.Console.WriteLine("Nobody likes your!");
+
+            // My original code threw an error. I'm including it below for reference. Need a little help on the specifics of why the ORDER of the IF/ELSE statement matters.
+            
+            // if (likes.Count == 0)
+            //     System.Console.WriteLine("Nobody likes you!");
+            // else if (likes.Count == 1)
+            //     System.Console.WriteLine("{0} likes your post.", likes[0]);
+            // else if (likes.Count == 2)
+            //     System.Console.WriteLine("{0} and {1} like your post.", likes[0], likes[1]);
+            // else
+            //     System.Console.WriteLine("{0}, {1} and {2} others like your post.", likes[0], likes[1], likes.Count - 2);
+
+            // !!UPDATE!! ---- For some reason I can comment out the code above and uncomment this code and it works now. I'm a bit lost. I'm not sure about how to debug this.
+            // I'm starting to think that the .dll was still the old version or something like that. If you have any insights on how the dotnet debugging/compiling works, I'd like to discuss this in further detail.
+        }
+    }
+}
---
+++ CSharpIntermediateCourse/Section2/Methods/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Methods
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            // var number = int.Parse("abc");
+            
+            int number;
+            var result = int.TryParse("abc", out number);
+            if (result)
+                System.Console.WriteLine(number);
+            else
+                System.Console.WriteLine("Conversion failed");
+        }
+
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+            System.Console.WriteLine(calculator.Add(1, 2));
+            System.Console.WriteLine(calculator.Add(1, 2, 3));
+            System.Console.WriteLine(calculator.Add(1, 2, 3, 4));
+            System.Console.WriteLine(calculator.Add(new int[] { 1, 2, 3, 4, 5}));
+        }
+
+        static void usePoints()
+        {
+            try
+            {
+            var point = new Point(10, 20);
+            point.Move(null);
+            System.Console.WriteLine("Point is at {0}, {1}", point.x, point.y);
+
+            point.Move(100, 200);
+            System.Console.WriteLine("Point is at {0}, {1}", point.x, point.y);
+                
+            }
+            catch (Exception)
+            {
+                System.Console.WriteLine("An unexpected error occurred.");
+            }
+ 
+        }
+    }
+}
