4afa013fe908a7365b06e4222455b6a69021870a
lostinthestack <lostinthestack@protonmail.com>
2021-09-01 14:16:12 -0500

    Update .gitignore

---
+++ CSharpIntermediateCourse/README.md
@@ -0,0 +1,1 @@
+# Intermediate course work
---
+++ scratchpad/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Code_Coach_Challenge
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int[] seq = {1, 2, 3, 4, 5, 6, 7, 8};
+
+            var oddOrEven = seq.Length%2;
+
+            if (oddOrEven == 1)
+                System.Console.WriteLine("Odd");
+            else
+                System.Console.WriteLine("Even");
+        }
+    }
+}
